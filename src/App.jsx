08d6eeca2e7056ee4b7c32ebe698fb20089e10c3
alexa109445 <alemanolache109@gmail.com>
2026-06-07 18:55:37 +0200

    introHome

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Home from './pages/Home.jsx';
+function App() {
+  return (
+    <div>
+        <Home />
+    </div>)
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../style/homestyle.css';
+
+function Home() {
+  return (
+    <section className="home"> 
+     <div className="introHome d-flex justify-content-center align-items-center " >
+      <div className='home-text-wrapper text-center'>
+      <h1 className="text-white">Alexandra Manolache</h1>
+      <h2 className='text-white'>Full-Stack Developer 👩🏻‍💻</h2>
+      </div>
+     </div>
+    </section>
+  );
+}
+
+export default Home;
